eba4e98b084331e3c0948199f5881ddabaa6fb22
dautiep <dautiep123@gmail.com>
2020-07-17 21:36:38 +0700

    gop code  17/7

--- resources/views/permission/create.blade.php
+++ resources/views/permission/create.blade.php
@@ -35,7 +35,7 @@
 								<!-- general form elements -->
 								<div class="card card-warning">
 									<div class="card-header">
-										<h3 class="card-title">Thông tin chung</h3>
+								     		<h3 class="card-title">Thông tin chung</h3>
 									</div>
 									<div class="card-body">
 										<form role="form" action="{{route('permissions.store')}}" method="post">
