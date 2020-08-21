1986194b0f5dfdd234c9f9b9344849eddbd59fe6
dautiep <dautiep123@gmail.com>
2020-08-21 23:33:57 +0700

    full code ver 2.0

--- resources/views/layouts/layout_admin/head_admin.blade.php
+++ resources/views/layouts/layout_admin/head_admin.blade.php
@@ -7,16 +7,16 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{URL::asset('public/AdminLTE-master/plugins/fontawesome-free/css/all.min.css')}}">
+  <link rel="stylesheet" href="{{URL::asset('public/plugins/fontawesome-free/css/all.min.css')}}">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Tempusdominus Bbootstrap 4 -->
-  <link rel="stylesheet" href="{{URL::asset('public/AdminLTE-master/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
+  <link rel="stylesheet" href="{{URL::asset('public/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
   <!-- DataTables -->
-  <link rel="stylesheet" href="{{URL::asset('public/AdminLTE-master/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
-  <link rel="stylesheet" href="{{URL::asset('public/AdminLTE-master/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
+  <link rel="stylesheet" href="{{URL::asset('public/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
+  <link rel="stylesheet" href="{{URL::asset('public/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
   <!-- Theme style -->
-  <link rel="stylesheet" href="{{URL::asset('public/AdminLTE-master/dist/css/adminlte.min.css')}}">
+  <link rel="stylesheet" href="{{URL::asset('public/dist/css/adminlte.min.css')}}">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
   <!--Crop image-->
--- resources/views/layouts/layout_admin/script_admin.blade.php
+++ resources/views/layouts/layout_admin/script_admin.blade.php
@@ -1,22 +1,22 @@
 <!-- jQuery -->
-<script src="{{URL::asset('public/AdminLTE-master/plugins/jquery/jquery.min.js')}}"></script>
+<script src="{{URL::asset('public/plugins/jquery/jquery.min.js')}}"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="{{URL::asset('public/AdminLTE-master/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+<script src="{{URL::asset('public/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- Bootstrap 4 -->
-<script src="{{URL::asset('public/AdminLTE-master/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{URL::asset('public/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 <!-- DataTables -->
-<script src="{{URL::asset('public/AdminLTE-master/plugins/datatables/jquery.dataTables.min.js')}}"></script>
-<script src="{{URL::asset('public/AdminLTE-master/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
-<script src="{{URL::asset('public/AdminLTE-master/plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
-<script src="{{URL::asset('public/AdminLTE-master/plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
+<script src="{{URL::asset('public/plugins/datatables/jquery.dataTables.min.js')}}"></script>
+<script src="{{URL::asset('public/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+<script src="{{URL::asset('public/plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{URL::asset('public/plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
 <!-- AdminLTE App -->
-<script src="{{URL::asset('public/AdminLTE-master/dist/js/adminlte.min.js')}}"></script>
+<script src="{{URL::asset('public/dist/js/adminlte.min.js')}}"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="{{URL::asset('public/AdminLTE-master/dist/js/demo.js')}}"></script>
+<script src="{{URL::asset('public/dist/js/demo.js')}}"></script>
 <!--crop image-->
 <script src="{{URL::asset('public/js/croppie.js')}}"></script>
 <!-- Toastr -->
